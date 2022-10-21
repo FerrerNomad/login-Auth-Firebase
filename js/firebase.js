bffd488e06af7b5993e606958bb4ae62ec3c4ff2
FerrerNomad <ferrernomad@outlook.com>
2022-10-21 09:22:40 -0300

    upload of files

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyALV6lAmSZhai18EeMgUP3IniNbeIWGa14",
+  authDomain: "loginfirebase-c6d7b.firebaseapp.com",
+  projectId: "loginfirebase-c6d7b",
+  storageBucket: "loginfirebase-c6d7b.appspot.com",
+  messagingSenderId: "90610596950",
+  appId: "1:90610596950:web:c1b2b38c2be3a61a970ff7",
+  measurementId: "G-L52V4RT9DT"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
